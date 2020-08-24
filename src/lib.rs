d6d56d31c25069b9fbfe64e32c3609d05206b45a
OliverBrotchie <o.brotchie@gmail.com>
2020-08-24 12:39:55 +0100

    initial push

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+use rayon::prelude::*;
+use reqwest::Url;
+use select::document::Document;
+use select::predicate::Name;
+use select::predicate::Predicate;
+use std::collections::HashSet;
+use std::io::Error as IoErr;
+use std::path::Path;
+use wasm_bindgen::prelude::*;
+
+
+#[derive(Debug)]
+enum Error {
+    Write { url: String, e: IoErr },
+    Fetch { url: String, e: reqwest::Error },
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl<S: AsRef<str>> From<(S, IoErr)> for Error {
+    fn from((url, e): (S, IoErr)) -> Self {
+        Error::Write {
+            url: url.as_ref().to_string(),
+            e,
+        }
+    }
+}
+
+impl<S: AsRef<str>> From<(S, reqwest::Error)> for Error {
+    fn from((url, e): (S, reqwest::Error)) -> Self {
+        Error::Fetch {
+            url: url.as_ref().to_string(),
+            e,
+        }
+    }
+}
+
+fn get_links_from_html(html: &str, origin: &str) -> HashSet<String> {
+    
+    let normalize_url = |url: &str| -> Option<String> {
+        let new_url = Url::parse(url);
+        let temp = origin;
+
+        let origin_url = &origin[origin.find("://").unwrap()+3 .. 
+            if origin[origin.find("://").unwrap()+3 ..].contains("/") {
+                origin.find("/").unwrap() - 1
+            } else {
+                origin.len() 
+            }];
+        
+        match new_url {
+            Ok(new_url) => {
+                if let Some(origin_url) = new_url.host_str() {
+                    Some(url.to_string())
+                } else {
+                    None
+                }
+            }
+            Err(_e) => {
+                // Relative urls are not parsed by Reqwest
+                if url.starts_with('/') {
+                    Some(format!("{}{}",temp,url))
+                } else {
+                    None
+                }
+            }
+        }
+    };
+    
+    return Document::from(html)
+        .find(Name("a").or(Name("link")))
+        .filter_map(|n| n.attr("href"))
+        .filter(has_extension)
+        .filter_map(normalize_url)
+        .collect::<HashSet<String>>();
+}
+
+
+async fn fetch_url(url: &str) -> Result<String, reqwest::Error> {
+    let mut res = reqwest::get(url).await?.map_err(|e| (url, e))?;
+    let mut body = String::new();
+    res.read_to_string(&mut body).map_err(|e| (url, e))?;
+    Ok(body)
+}
+
+fn has_extension(url: &&str) -> bool {
+    Path::new(&url).extension().is_none()
+}
+
+
+#[wasm_bindgen]
+pub fn crawl(input: &str) -> String {
+    let client = reqwest::Client::new();
+
+    let body = fetch_url(&client, input).unwrap();
+
+    let mut visited = HashSet::new();
+    visited.insert(input.to_string());
+    let found_urls = get_links_from_html(&body,input);
+
+    let mut new_urls = found_urls
+        .difference(&visited)
+        .map(|x| x.to_string())
+        .collect::<HashSet<String>>();
+    while !new_urls.is_empty() {
+        let (found_urls, errors): (Vec<Result<HashSet<String>>>, Vec<_>) = new_urls
+            .par_iter()
+            .map(|url| -> Result<HashSet<String>> {
+                let body = fetch_url(&client, url)?;
+                let links = get_links_from_html(&body, input);
+                println!("Visited: {} found {} links", url, links.len());
+                Ok(links)
+            })
+            .partition(Result::is_ok);
+
+        visited.extend(new_urls);
+        new_urls = found_urls
+            .into_par_iter()
+            .map(Result::unwrap)
+            .reduce(HashSet::new, |mut acc, x| {
+                acc.extend(x);
+                acc
+            })
+            .difference(&visited)
+            .map(|x| x.to_string())
+            .collect::<HashSet<String>>();
+        println!("New urls: {}", new_urls.len());
+        println!(
+            "Errors: {:#?}",
+            errors
+                .into_iter()
+                .map(Result::unwrap_err)
+                .collect::<Vec<Error>>()
+        )
+    }
+    println!("{:?}", visited);
+    let output: Vec<_> = visited.into_iter().collect();
+
+    return output.join(", ");
+}
